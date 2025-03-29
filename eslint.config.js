04c9fa21fe4da9eeb3e1981142b3cb3e0fb365ab
Alex <37328596+aliakseitokarev@users.noreply.github.com>
2025-03-29 16:29:27 +0100

    fix: change eslint.config to run linter correctly (#26)

--- eslint.config.js
+++ eslint.config.js
@@ -1,25 +1,29 @@
 const globals = require('globals');
 const pluginJs = require('@eslint/js');
-const stylistic = require('@stylistic/eslint-plugin');
 
 /** @type {import('eslint').Linter.Config[]} */
-const config = [
-  {
-    plugins: {
-      '@stylistic': stylistic,
-    },
-    languageOptions: {
-      parserOptions: {
-        ecmaVersion: 6,
-      },
-      globals: Object.assign({}, globals.browser, globals.node, globals.jest),
-    },
-    rules: {
-      '@stylistic/indent': ['error', 2, { SwitchCase: 1 }],
-      '@stylistic/semi': ['error', 'always'],
-    },
-  },
-  pluginJs.configs.recommended,
-];
 
-module.exports = config;
+const loadConfig = async () => {
+  const stylistic = await import('@stylistic/eslint-plugin');
+
+  return [
+    {
+      plugins: {
+        '@stylistic': stylistic.default,
+      },
+      languageOptions: {
+        parserOptions: {
+          ecmaVersion: 6,
+        },
+        globals: Object.assign({}, globals.browser, globals.node, globals.jest),
+      },
+      rules: {
+        '@stylistic/indent': ['error', 2, { SwitchCase: 1 }],
+        '@stylistic/semi': ['error', 'always'],
+      },
+    },
+    pluginJs.configs.recommended,
+  ];
+};
+
+module.exports = loadConfig();
